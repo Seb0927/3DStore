b09d6f7d4135c03bbc53f57731001600335b21c4
Jose Manuel Palma Oquendo <80994908+JoseManuel2005@users.noreply.github.com>
2024-10-15 23:39:12 -0500

    Firebase: editing firebase config file

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-const provider = new GoogleAuthProvider();
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,4 +15,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export { app };
+// Initialize Firebase Authentication and the Google provider
+const auth = getAuth(app);
+// const provider = new GoogleAuthProvider();
+
+export { auth, signInWithPopup };
